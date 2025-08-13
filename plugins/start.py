c61f309281452a6af9e9df30b77cde73e9622691
Flix_botz <99806982+Priyanshu1057@users.noreply.github.com>
2025-08-13 17:30:31 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -177,7 +177,6 @@ async def start_command(client: Client, message: Message):
                 print(f"Error updating notification with 'Get File Again' button: {e}")
     else:
         reply_markup = InlineKeyboardMarkup(
-            reply_markup = InlineKeyboardMarkup(
             [
                     [InlineKeyboardButton("• ᴍᴏʀᴇ ᴄʜᴀɴɴᴇʟs •", url="https://t.me/Nova_Flix/50")],
 
